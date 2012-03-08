5ac2adc5cf6aad77eea4b246f3a4fc0ebdda1f28
Jelmer <jelmer@ikhoefgeen.nl>
2012-03-08 10:03:10 +0100

    Add spread of light (feedback from student-assistent)

--- openglframework.c
+++ openglframework.c
@@ -258,7 +258,9 @@ void initLights()
 {
     GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[] = { 50.0 };
-    GLfloat light_position[] = { -200.0, 600.0, 1500.0 };
+
+    // position, spread of light
+    GLfloat light_position[] = { -200.0, 600.0, 1500.0, 1.0};
     
     glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
